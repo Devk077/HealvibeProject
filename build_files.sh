de4e9767a25cee2572ecfaa7408708877054e327
KAPADIA DEV SHAILESHKUMAR <128740434+Devk077@users.noreply.github.com>
2024-04-01 02:10:55 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 # build_files.sh
-pip install -r requirements.txt
-python manage.py collectstatic
+echo "Building files..."
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "Files built successfully."
